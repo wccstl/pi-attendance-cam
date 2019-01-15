3997451bb6a7f00e2dbc0df315346c8eb0a8c30c
Elliot J. Voris <elliot@voris.me>
2019-01-15 10:41:45 -0600

    Proof of concept for pysmb.
    Just copying a file for now. But, it's working well. Let's try this
    thing for real now.
    
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- transfer.py
+++ transfer.py
@@ -1,5 +1,17 @@
 import yaml
+import tempfile
+import os
+import sys
 
 from datetime import datetime
+from smb.SMBConnection import SMBConnection
 
 config = yaml.safe_load(open(os.path.join(sys.path[0], "config.yml")))
+
+conn = SMBConnection(config['UserID'], config['password'], config['client_machine_name'], config['server_name'])
+assert conn.connect(config['server_ip'])
+
+file_obj = tempfile.NamedTemporaryFile(delete=False)
+file_attributes, filesize = conn.retrieveFile('Data', '/Group.jpg', file_obj)
+
+file_obj.close()
